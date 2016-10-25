22b6c6cb986e8377d9af4f3dd691681b39fa0052
Mitchell Dowd <mitchell.dowd@live.com>
2016-10-26 09:50:38 +1100

    Adding more unit tests.

--- tests/main.cpp
+++ tests/main.cpp
@@ -125,14 +125,99 @@ TEST(Object_copyConstructorIsSameObject)
 	ASSERT(a == b);
 }
 
+TEST(Object_moveConstructor)
+{
+	jni::Object a = jni::Class("java/lang/String").newInstance();
+	jni::Object b = std::move(a);
+
+	ASSERT(a.isNull());
+	ASSERT(!b.isNull());
+}
+
+TEST(Object_call)
+{
+	jni::Class Integer("java/lang/Integer");
+	jni::method_t intValue = Integer.getMethod("intValue", "()I");
+	jni::Object i = Integer.newInstance(100);
+
+	ASSERT(i.call<int>(intValue) == 100);
+}
+
+TEST(Object_call_byName)
+{
+	jni::Object i = jni::Class("java/lang/Integer").newInstance(100);
+
+	ASSERT(i.call<int>("intValue") == 100);
+}
+
+TEST(Object_call_withArgs)
+{
+	jni::Class String("java/lang/String");
+	jni::method_t charAt = String.getMethod("charAt", "(I)C");
+	jni::Object str = String.newInstance("Testing");
+
+	ASSERT(str.call<wchar_t>(charAt, 1) == L'e');
+}
+
+TEST(Object_call_byNameWithArgs)
+{
+	jni::Object str = jni::Class("java/lang/String").newInstance("Testing");
+
+	ASSERT(str.call<wchar_t>("charAt", 1) == L'e');
+}
+
+TEST(Arg_bool)
+{
+	jni::String str = jni::Class("java/lang/String").call<jni::String>("valueOf", true);
+
+	ASSERT(str == "true");
+}
+
+TEST(Arg_wchar)
+{
+	jni::String str = jni::Class("java/lang/String").call<jni::String>("valueOf", L'X');
+
+	ASSERT(str == "X");
+}
+
+TEST(Arg_double)
+{
+	jni::String str = jni::Class("java/lang/String").call<jni::String>("valueOf", 123.0);
+
+	ASSERT(str == "123.0");
+}
+
+TEST(Arg_float)
+{
+	jni::String str = jni::Class("java/lang/String").call<jni::String>("valueOf", 123.0f);
+
+	ASSERT(str == "123.0");
+}
+
+TEST(Arg_int)
+{
+	jni::String str = jni::Class("java/lang/String").call<jni::String>("valueOf", 123);
+
+	ASSERT(str == "123");
+}
+
+TEST(Arg_longLong)
+{
+	jni::String str = jni::Class("java/lang/String").call<jni::String>("valueOf", 123LL);
+
+	ASSERT(str == "123");
+}
+
 int main()
 {
+	// jni::Vm Tests
 	RUN_TEST(Vm_detectsJreInstall);
+	RUN_TEST(VM_notAllowedMultipleVms);
 
 	{
 		jni::Vm vm;
 
-		RUN_TEST(VM_notAllowedMultipleVms);
+		// jni::Class Tests
 		RUN_TEST(Class_findByName);
 		RUN_TEST(Class_newInstance);
 		RUN_TEST(Class_newInstance_withArgs);
@@ -142,8 +227,23 @@ int main()
 		RUN_TEST(Class_get_staticField);
 		RUN_TEST(Class_get_staticField_byName);
 		RUN_TEST(Class_call_staticMethod_byName);
+
+		// jni::Object Tests
 		RUN_TEST(Object_defaultConstructor_isNull);
 		RUN_TEST(Object_copyConstructorIsSameObject);
+		RUN_TEST(Object_moveConstructor);
+		RUN_TEST(Object_call);
+		RUN_TEST(Object_call_byName);
+		RUN_TEST(Object_call_withArgs);
+		RUN_TEST(Object_call_byNameWithArgs);
+
+		// Argument Type Tests
+		RUN_TEST(Arg_bool);
+		RUN_TEST(Arg_wchar);
+		RUN_TEST(Arg_double);
+		RUN_TEST(Arg_float);
+		RUN_TEST(Arg_int);
+		RUN_TEST(Arg_longLong);
 	}
 
 	std::cout << "Press a key to continue..." << std::endl;
