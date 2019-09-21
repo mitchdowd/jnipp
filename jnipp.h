b5f476bba5b7cb9b0d2d9c5d502e9cadb67902cf
huangxiaozi <opencai@126.com>
2019-09-21 21:09:40 +0800

    fix bug

--- jnipp.h
+++ jnipp.h
@@ -539,7 +539,7 @@ namespace jni
          */
         template <class TReturn>
         TReturn call(const char* name) const {
-            method_t method = getMethod(name, ("()" + internal::valueSig((TReturn*) nullptr)).c_str());
+            method_t method = callStaticMethod(name, ("()" + internal::valueSig((TReturn*) nullptr)).c_str());
             return call<TReturn>(method);
         }
 
